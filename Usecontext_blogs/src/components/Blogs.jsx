b74a1ab681bd5bea7d789037c239439f1804fe29
SAH1L19 <146871006+SAH1L19@users.noreply.github.com>
2024-06-15 18:27:47 +0530

    Add files via upload

--- Usecontext_blogs/src/components/Blogs.jsx
+++ Usecontext_blogs/src/components/Blogs.jsx
@@ -1,41 +1,27 @@
-import React,{useContext} from 'react';
-import { AppContext } from '../context/AppContext';
-import Spinner from './Spinner';
-import "./Blogs.css";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import Spinner from "./Spinner";
+import BlogDetails from "./BlogDetails";
 
+const Blogs = () => {
+  const { posts, loading } = useContext(AppContext);
+  return ( 
+    <div className="my-[100px]">
+      <div className="flex flex-col gap-y-10 my-4">
+        {loading ? (
+          <Spinner />
+        ) : posts.length === 0 ? (
+          <p className="font-bold text-3xl text-center my-[200px]">
+            Data Not Found
+          </p>
+        ) : (
+          posts.map((post) => (
+            <BlogDetails key={post.id} post={post} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
 
-const Blogs=()=>{
-
-    const { posts, loading } = useContext(AppContext);
-    return(
-        <div className='w-11/12 max-w-[650px] py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px]'>
-        {
-            loading?
-
-            (<Spinner/>):
-            (
-                  posts.length===0?
-                   (<div><p>No Posts Found</p>
-                   </div>):
-    
-                   (posts.map((post)=>(
-                    <div key={post.id}>
-                    <p className=' text-lg font-bold'>{post.title}</p>
-                    <p className='text-sm mt-[4px]'>
-                        By<span className='italic'>{post.author}</span>on<span className='underline font-bold'>{post.category}</span>
-                    </p>
-                        <p className='text-sm mt-[3px]'>Posted on {post.date}</p>
-                            <p className='text-md mt-[14px]'>{post.content}</p>
-                            <div className='flex gap-x-3'>
-                                {post.tags.map((tag,index)=>{
-                                    return <span className='text-blue-700 underline font-bold text-xs' key={index}>{`#${tag}`}</span>
-                                    })}
-                            </div>      
-                    </div>) ))
-            )
-        }
-
-        </div>
-    )
-}
 export default Blogs;
--- Usecontext_blogs/src/index.js
+++ Usecontext_blogs/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AppContext } from "./context/AppContext";
 import AppContextProvider from "./context/AppContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- 
+  <BrowserRouter> 
     <AppContextProvider>
-      <App />
-    </AppContextProvider>
+      
+    <App />
+   
+  </AppContextProvider>
+  </BrowserRouter>
+   
   
 );
