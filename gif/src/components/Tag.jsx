d9949c131f427ab9eacb4c4b20f7025c4019be8e
SAH1L19 <146871006+SAH1L19@users.noreply.github.com>
2024-06-23 15:13:14 +0530

    Update Tag.jsx

--- gif/src/components/Tag.jsx
+++ gif/src/components/Tag.jsx
@@ -28,7 +28,7 @@ const Tag = () => {
 
 
   return (
-    <div className="w-1/2  bg-blue-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+    <div className="w-1/2  bg-blue-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px] py-5">
       <h1 className="text-3xl uppercase underline font-bold">Random {tag} Gif</h1>
       {
         loading ? (<Spinner />) : (<img src= {gif} width="450" />)
@@ -38,7 +38,7 @@ const Tag = () => {
 
       <button
         onClick={() => fetchData(tag)}
-        className="w-10/12 bg-white text-xl py-2 rounded-lg font-bold mb-[20px]"
+        className="w-10/12 bg-white text-xl py-2 rounded-lg font-bold "
       >
         Generate
       </button>
